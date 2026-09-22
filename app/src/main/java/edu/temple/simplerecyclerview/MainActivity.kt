bb706713a9ceac6427f8c9cd41f8d07c823850da
bella-otter <tup17855@temple.edu>
2026-09-22 11:59:30 -0400

    added lambda, onClickListener, and functionality to lambda. Clicking on text size changes the text size of the top text

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -18,11 +18,12 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
-        val callback : (Int) -> Unit = {}
+        val callback  = {mySize:Int->
+            displayTextView.textSize = mySize.toFloat()}
 
 
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+        recyclerView.adapter = NumberDisplayAdapter(numbers, callback)
 
 
     }
